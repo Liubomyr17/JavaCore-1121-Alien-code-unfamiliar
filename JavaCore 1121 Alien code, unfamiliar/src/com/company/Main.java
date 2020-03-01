2b6915e2579ca42ff37fe7e0587980d3ba054d82
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-03-01 22:04:29 +0200

    Add files via upload

---
+++ JavaCore 1121 Alien code, unfamiliar/src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+/*
+
+1121 Alien code, unfamiliar
+Fix inheritance in classes: (Cat, Dog, Pet, House, Airplane classes).
+
+Requirements:
+1. The Pet class should not be inherited from our classes.
+2. The Cat class must be inherited from the Pet class.
+3. The class Dog must be inherited from the class Pet.
+4. The House class should not be inherited from our classes.
+5. The Airplane class should not be inherited from our classes.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+    }
+    public class Pet {
+    }
+    public class Cat extends Pet {
+    }
+    public class Dog extends Pet {
+    }
+    public class House {
+    }
+    public class Airplane {
+    }
+}
+
+
+
